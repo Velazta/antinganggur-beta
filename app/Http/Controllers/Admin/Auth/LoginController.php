70c734d54b29c01b5e2f251db83bcb7e9f6a3715
RavelinLuth <ravelinlutfhanrlsp@gmail.com>
2025-06-11 18:03:48 +0700

    fixing bug session authentification karena user terlogout juga

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -38,8 +38,6 @@ class LoginController extends Controller
     public function logout(Request $request)
     {
         Auth::guard('admin')->logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
         return redirect()->route('admin.login');
     }
 }
